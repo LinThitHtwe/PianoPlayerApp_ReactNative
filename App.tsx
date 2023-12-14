efa27d5ddcdb89020cdcb6ba0c48daeb7db28914
LinThit27 <linthit2745@gmail.com>
2023-12-14 16:42:35 +0630

    [init] init files and basic ui design

--- App.tsx
+++ App.tsx
@@ -1,118 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'];
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#B7E5C5',
+        padding: 20,
+        height: 'auto',
+        width: 'auto',
+        alignContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello</Text>
+      <View style={{gap: 2, flexDirection: 'row'}}>
+        {notes.map((note, index) => (
+          <TouchableOpacity
+            key={index}
+            style={{
+              flexDirection: 'column',
+              height: 300,
+              width: 75,
+              backgroundColor: '#fff',
+              margin: 0,
+              borderRadius: 10,
+              position: 'relative',
+              justifyContent: 'flex-end',
+            }}>
+            <Text
+              style={{
+                color: '#000',
+                textAlign: 'center',
+                padding: 30,
+                fontSize: 20,
+              }}>
+              {note}
+            </Text>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#000',
+                height: 180,
+                width: 38,
+                position: 'absolute',
+                top: -5,
+                left: -4,
+                zIndex: 10,
+              }}>
+              <Text style={{color: '#000'}}>HEL</Text>
+            </TouchableOpacity>
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+// import Orientation from 'react-native-orientation-locker';
 
+// Orientation.lockToLandscape();
 AppRegistry.registerComponent(appName, () => App);
