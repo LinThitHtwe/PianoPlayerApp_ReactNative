5af56e5cbb8f101dde4b41a9c774c4298d703330
LinThit27 <linthit2745@gmail.com>
2023-12-14 17:02:53 +0630

    [modify] modify ui, finish all basic look and changes in looping

--- App.tsx
+++ App.tsx
@@ -2,56 +2,98 @@ import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 
 function App(): React.JSX.Element {
-  const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A', 'B', 'C'];
+  const notes = [
+    'A',
+    'Bb',
+    'B',
+    'C',
+    'C#',
+    'D',
+    'Eb',
+    'E',
+    'F',
+    'F#',
+    'G',
+    'G#',
+    'A',
+    'Bb',
+    'B',
+    'C',
+  ];
 
   return (
     <View
       style={{
         flex: 1,
         backgroundColor: '#B7E5C5',
-        padding: 20,
+        paddingHorizontal: 20,
+
         height: 'auto',
         width: 'auto',
         alignContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello</Text>
+      <Text
+        style={{
+          color: '#000',
+          fontSize: 25,
+          fontWeight: '700',
+          paddingBottom: 12,
+        }}>
+        Press any key to play piano •⩊•
+      </Text>
       <View style={{gap: 2, flexDirection: 'row'}}>
         {notes.map((note, index) => (
-          <TouchableOpacity
-            key={index}
-            style={{
-              flexDirection: 'column',
-              height: 300,
-              width: 75,
-              backgroundColor: '#fff',
-              margin: 0,
-              borderRadius: 10,
-              position: 'relative',
-              justifyContent: 'flex-end',
-            }}>
-            <Text
-              style={{
-                color: '#000',
-                textAlign: 'center',
-                padding: 30,
-                fontSize: 20,
-              }}>
-              {note}
-            </Text>
-            <TouchableOpacity
-              style={{
-                backgroundColor: '#000',
-                height: 180,
-                width: 38,
-                position: 'absolute',
-                top: -5,
-                left: -4,
-                zIndex: 10,
-              }}>
-              <Text style={{color: '#000'}}>HEL</Text>
-            </TouchableOpacity>
-          </TouchableOpacity>
+          <View>
+            {note.length == 1 && (
+              <TouchableOpacity
+                key={index}
+                style={{
+                  flexDirection: 'column',
+                  height: 300,
+                  width: 72,
+                  backgroundColor: '#fff',
+                  margin: 0,
+                  borderRadius: 10,
+                  justifyContent: 'flex-end',
+                }}>
+                <Text
+                  style={{
+                    color: '#000',
+                    textAlign: 'center',
+                    padding: 30,
+                    fontSize: 20,
+                  }}>
+                  {note}
+                </Text>
+              </TouchableOpacity>
+            )}
+
+            {note.length > 1 && (
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#000',
+                  height: 180,
+                  width: 48,
+                  position: 'absolute',
+                  top: -5,
+                  left: -18,
+                  zIndex: 10,
+                  borderRadius: 6,
+                  justifyContent: 'flex-end',
+                }}>
+                <Text
+                  style={{
+                    color: '#fff',
+                    padding: 10,
+                    textAlign: 'center',
+                    fontSize: 18,
+                  }}>
+                  {note}
+                </Text>
+              </TouchableOpacity>
+            )}
+          </View>
         ))}
       </View>
     </View>
